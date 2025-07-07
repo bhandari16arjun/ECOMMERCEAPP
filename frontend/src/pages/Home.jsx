ceac880eef0161ca215ea37bcf6558812607f85a
Arjun Bhandari <arjunmbhandari16@gmail.com>
2025-07-07 21:52:44 +0530

    initial commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import BestSeller from "../components/BestSeller"
 import Hero from "../components/Hero"
+import LatestCollection from "../components/LatestCollection"
+import NewsletterBox from "../components/NewsletterBox"
+import OurPolicy from "../components/OurPolicy"
+import Footer from "../components/Footer"
 
 export default function Home(){
     return (
-       <Hero></Hero>
+      <div>
+           <Hero></Hero>
+           <LatestCollection/>
+           <BestSeller/>
+           <OurPolicy/>
+           <NewsletterBox/>
+           <Footer/>
+      </div>  
     )
 }
